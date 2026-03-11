3b4abda3d2adf05f62b78c42c5862f239150faa6
salima <sahisalima77@gmail.com>
2026-03-11 13:47:40 +0100

    create model user

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+
+const userScheme=new mongoose.schema(
+  {  name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    passeword:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["user","admin"],
+        default:"user",
+        required:true,
+
+    }
+},
+{
+    timestamps:true
+},
+)
+
+const Abonnement=mongoose.model("User",userScheme)
+
+export default Abonnement;
